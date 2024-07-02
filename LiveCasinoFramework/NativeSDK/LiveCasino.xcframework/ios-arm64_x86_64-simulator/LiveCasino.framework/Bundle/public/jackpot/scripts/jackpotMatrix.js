c474987e4b55cc20afdc397e66b2dd1c7ad429ec
AlexGP <gabrielalexandro.garciapinzon@gmail.com>
2024-07-02 13:23:52 -0600

    Removing NativeSDK from repo
    Removing NativeSDK from repo

--- LiveCasinoFramework/NativeSDK/LiveCasino.xcframework/ios-arm64_x86_64-simulator/LiveCasino.framework/Bundle/public/jackpot/scripts/jackpotMatrix.js
+++
@@ -1,1 +0,0 @@
-!function(t){"use strict";function i(t){var i=this;i._matrix=[],i.size=t,i.forEachCoin(function(t,o,n){i._matrix[o+"_"+n]=new JackpotCoin({x:o,y:n})})}i.prototype.forEachCoin=function(t){var i,o;for(i=1;i<=this.size.rows;i++)for(o=1;o<=this.size.cols;o++)t(this._matrix[i+"_"+o],i,o)},i.prototype.getCoin=function(t,i){return this._matrix[t+"_"+i]},t.JackpotMatrix=i}(window);
--- LiveCasinoFramework/NativeSDK/Tools.xcframework/ios-arm64_x86_64-simulator/Tools.framework/Headers/Tools-NS_OPTIONS.h
+++
@@ -1,9 +0,0 @@
-//
-//  Tools-NS_OPTIONS.h
-//  Tools
-//
-//  Created by Daniil Mazepin on 9/5/19.
-//  Copyright © 2019 playtech. All rights reserved.
-//
-
-#define OPTIONS_HAS_VALUE(options, value) (((options) & (value)) == (value))
